fd6fddd971b7a48b53004182db3badc820e47d40
starkbak <Salim@mgial.com>
2024-09-02 18:47:47 +0430

    resume added to site stactic files

--- components/atoms/DownloadResumeButton.tsx
+++ components/atoms/DownloadResumeButton.tsx
@@ -9,7 +9,8 @@ const DownloadResumeButton: NextComponentType<NextPageContext, {}, Props> = (
   return (
     <div className="m-10 text-white sm:text-center lg:text-left">
       <Link
-        href={""}
+        href={"/SalimBaktashResume.pdf"}
+        target="_blank"
         className="transition-shadow duration-700 ease-in-out bg-cyan-900 p-5 rounded-2xl text-white font-bold hover:shadow-lg hover:shadow-gray-500 "
       >
         Download Resume
