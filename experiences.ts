ecc1d4f517275c48c594c4549deda30d116e87d4
StarkBak <sstechnologyindtec@gmail.com>
2024-09-12 23:24:30 +0430

    ttt logo updated

--- experiences.ts
+++ experiences.ts
@@ -4,8 +4,7 @@ export const experiences = [
     date: "2021 - 2024",
     description:
       "I started working at the Top In Town Technology in 2021 as a Software Development Instructor. In Top in Town Technology I thought different technologies.",
-    imageLink:
-      "https://scontent.fhea4-1.fna.fbcdn.net/v/t39.30808-1/278517051_1625962901110269_343389607696367045_n.jpg?stp=dst-jpg_s200x200&_nc_cat=108&ccb=1-7&_nc_sid=f4b9fd&_nc_ohc=h0zVyY608twQ7kNvgE5CKoU&_nc_ht=scontent.fhea4-1.fna&oh=00_AYAWFEBEcwHpIav-iDkjj3LlLm-xolRswqmIZrAKTXHSCg&oe=66DB842C",
+    imageLink: "/tttlogo.jpg",
   },
   {
     title: "Full Stack Developer at Listoli LLC",
@@ -19,7 +18,6 @@ export const experiences = [
     date: "1/6/2024 - until now",
     description:
       "After I came out from the Listoli LLC, I started teaching again in Top In Town Technology",
-    imageLink:
-      "https://scontent.fhea4-1.fna.fbcdn.net/v/t39.30808-1/278517051_1625962901110269_343389607696367045_n.jpg?stp=dst-jpg_s200x200&_nc_cat=108&ccb=1-7&_nc_sid=f4b9fd&_nc_ohc=h0zVyY608twQ7kNvgE5CKoU&_nc_ht=scontent.fhea4-1.fna&oh=00_AYAWFEBEcwHpIav-iDkjj3LlLm-xolRswqmIZrAKTXHSCg&oe=66DB842C",
+    imageLink: "/tttlogo.jpg",
   },
 ];
